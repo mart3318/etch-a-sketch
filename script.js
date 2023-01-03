ba85e5cc68ee7f2c5cad2fa781ccfa80ad8dbd55
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-01-03 12:02:51 -0600

    Create table element and append to document

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.createElement("div")
+/*
+function createTable() {
+    let table = document.createElement("table")
+    table.setAttribute("id", "sketchArea")
+    for (let i = 0; i < 16; i++) {
+        let row = table.insertRow(i)
+        for (let j = 0; j < 16; j++) {
+            let cell = row.insertCell(j)
+            cell.innerHTML = "🖤"
+        }
+    }
+    document.body.appendChild(table)
+}
+*/
